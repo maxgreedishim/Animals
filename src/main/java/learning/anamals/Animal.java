bfd1afb927a3a39bb6fa8995030f180ff503be14
Денис <89923002057@mail.ru>
2021-09-10 22:29:40 +0500

    комментарии к классу Animal

--- src/main/java/learning/anamals/Animal.java
+++ src/main/java/learning/anamals/Animal.java
@@ -1,67 +1,61 @@
 package learning.anamals;
 
-abstract class Animal implements Runnable {
-    private String name;
-    protected double weight;
-    private boolean gender;
+abstract class Animal implements Runnable { //абстракный класс, кроме того наследуется от интерфейса, потому что из него будет запущенн поток Thread.
+    private String name; // поле имя
+    private boolean gender; // поле пол.
+    protected double weight; // поле вес доступен всем классам наследникам (protected)
 
+    Animal() { // пустой конструктор
 
-
-    Animal() {
-
-        new Thread(this).start();
+        new Thread(this).start(); //создаем поток к текущему животному, стартуем
     }
 
-    Animal (String name) {
-        this();
-        this.name = name;
-        this.gender = true;
+    Animal (String name) { //коннструктор с одним параметром.
+        this(); //сначала вызываем конструктор без параметров
+        this.name = name;  //присваивание полю значение
+        this.gender = true; //по умолчанию пол мужской
 
     }
-    Animal (String name, double weight){
-        this();
-        this.name = name;
-        this.weight = weight;
-        this.gender = true;
+    Animal (String name, double weight){ // конструктор с другим наборов параметров
+        this(name); // конструктор обращается к другому конструктору
+        this.weight = weight; //присваивание полю значение
+
     }
-    Animal (String name, boolean gender) {
-        this();
-        this.name = name;
-        this.gender = gender;
+    Animal (String name, boolean gender) {// конструктор с другим наборов параметров
+        this(name);// конструктор обращается к другому конструктору
+        this.gender = gender;//присваивание полю значение
 
     }
 
 
-    Animal(String name, double weight, boolean gender) {
-        this();
-        this.name = name;
-        this.weight = weight;
+    Animal(String name, double weight, boolean gender) {// конструктор с другим наборов параметров
+        this(name, weight); // конструктор обращается к другому конструктору
         this.gender = gender;
     }
 
 
-    abstract String makeSound();
+    abstract String makeSound(); // абстракный метод, что говорит животное, должен(обязан) быть реализован классах потомках
 
-    void feed () {
+    void feed () { // метод кормления
 
-        weight += getPortion();
+        weight += getPortion(); //к весу + порцию кормления
     }
-    void slim () {
-        weight -= getPortion() * World.getSlimmingFactor();
+    void slim () { // метод худения с учетом фактора
+        weight -= getPortion() * World.getSlimmingFactor(); //набирает или теряем вес
     }
 
-    abstract double getPortion();
+    abstract double getPortion(); // у каждого животного разные порции должен(обязан) быть реализован классах потомках
 
     @Override
-    public String toString() {
+    public String toString() { // удобное представление инстанца ввиде строки
 
-        return  "Меня зовут " + name + ". " + makeSound() + "!";
+        return  "Меня зовут " + name + ". " + makeSound() + "!"; // понятная строка на русском языке
     }
     @Override
-    public void run () {
-        double startWeight = weight;
-        System.out.println(this);
-        while (weight > 0) {
+    public void run () { // мы обязаны написать это  метод потому что класс Runnable
+        double startWeight = weight; // присваивание переменной
+        System.out.println(this); // приветствие животного
+        while (weight > 0) { // цыкл
             feed();
             slim ();
 
@@ -74,8 +68,8 @@ abstract class Animal implements Runnable {
                 break;
             }
             System.out.println("Вес = " + weight + " " + name);
-            try {
-                Thread.sleep(500);
+            try { // обработка исключений
+                Thread.sleep(500); // задержка вывода
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
